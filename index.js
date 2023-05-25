8d5d3ce69d3947598aa01bb581741f11990e2e80
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-05-25 11:36:27 +0200

    Add files via upload
    completed navbar without animation part 3

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-$('.navbar-toggler').click(function(){
+var menuOpened = false;
+$('.navbar-toggler').click(function () {
     $('body').toggleClass("greyBg");
+    if (menuOpened == false) {
+        $('.navbar-toggler-icon').removeClass("ham");
+        $('.navbar-toggler-icon').addClass("cross");
+        menuOpened = true;
+    }
+    else {
+        $('.navbar-toggler-icon').removeClass("cross");
+        $('.navbar-toggler-icon').addClass("ham");
+        menuOpened = false;
+    }
 });
